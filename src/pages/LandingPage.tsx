533fd4c26e97618a8d1a2a9785f1d5bb9252ecf4
Greg Zwickl <gregzwickl@MacBook-Air-5.local>
2025-02-08 16:30:52 -0500

    indexing updates

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,10 +5,15 @@ import { MainFeatures } from '../components/landing/MainFeatures';
 import { Pricing } from '../components/landing/Pricing';
 import { ValueProps } from '../components/landing/ValueProps';
 import { Footer } from '../components/Footer';
+import { Helmet } from 'react-helmet';
 
 export const LandingPage: React.FC = () => {
   return (
     <div className="bg-white">
+      <Helmet>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex, nofollow" />
+      </Helmet>
       <Hero />
       <div id="features">
         <Features />
